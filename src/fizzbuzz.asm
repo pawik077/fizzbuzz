fd15ad1dfd5d98ace778457d39ebd5ed909a9cbf
pawik077 <pawik.r.s.077@gmail.com>
2023-03-13 15:22:36 +0100

    Working example

--- src/fizzbuzz.asm
+++ src/fizzbuzz.asm
@@ -3,7 +3,7 @@ section     .text
 global      _start 
 
 print_lf:
-    mov    rsi,lf ; LF
+    mov    rsi,lf   ; line feed 
     mov    rax,1    ; write syscall
     mov    rdi,1    ; stdout
     mov    rdx,1    ; string length
@@ -21,7 +21,7 @@ print_int:
     mov    r9, 2
     mov    rax,rsi
 digits_loop:
-    mov    rdx,0
+    xor    rdx,rdx
     mov    rcx,10   ; divide by 10
     div    rcx      ; result in rax, modulus in rdx
     
@@ -53,7 +53,7 @@ print_buzz:
     call   print_lf
     ret
 
-print_fizzbuzz
+print_fizzbuzz:
     mov    rsi,f
     call   print_str
     mov    rsi,b
@@ -62,8 +62,54 @@ print_fizzbuzz
     ret
 
 _start: 
-    call print_fizzbuzz
+    xor    r8,r8    ; zero r8, as counter
+main_loop:
+    inc    r8
+    xor    r9,r9    ; testing variable register
+    xor    rdx,rdx  ; set rdx to 0
 
+    mov    rax,r8   
+    mov    rcx,3    ; divide by 3
+    div    rcx
+    cmp    rdx,0    ; check modulus
+    jnz    not_div_3
+    or     r9,1     ; r9 + 1 if divisible by 3
+not_div_3:
+    xor    rdx,rdx
+    mov    rax,r8
+    mov    rcx,5    ; divide by 5
+    div    rcx
+    cmp    rdx,0    ; check modulus
+    jnz not_div_5
+    or     r9,2     ; r9 + 2 if divisible by 5
+not_div_5:
+    ; r9 == 1: fizz, r9 == 2: buzz, r9 == 3: fizzbuzz, r9 == 0: number
+    cmp    r9,0
+    je     number
+    cmp    r9,1
+    je     fizz
+    cmp    r9,2
+    je     buzz
+    cmp    r9,3
+    je     fizzbuzz
+number:
+    mov    rsi,r8
+    call   print_int
+    jmp    next
+fizz:
+    call   print_fizz
+    jmp    next
+buzz:
+    call   print_buzz
+    jmp    next
+fizzbuzz:
+    call   print_fizzbuzz
+    jmp    next
+next:
+    cmp    r8,total
+    je     exit
+    jmp    main_loop
+exit:
     mov    rax,60
     mov    rdi,0
     syscall
@@ -72,5 +118,6 @@ section    .data
 f     db     'Fizz'
 b     db     'Buzz'
 lf    db     10
+total equ     100
 section    .bss
 buf   resb   3
